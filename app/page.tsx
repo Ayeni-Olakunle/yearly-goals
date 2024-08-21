085168cae777aba918cfbf15f24a57e261478fc6
ayeni olakunle <ayeniolakunle98@gmail.com>
2024-08-21 17:38:27 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -5,27 +5,57 @@ import { FcGoogle } from "react-icons/fc";
 
 export default function Login() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[#407BFF]">
-      <div className="w-[500px] bg-[white] [box-shadow:0px_7px_10px_6px_#00000033] rounded-[10px] overflow-hidden">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-[#407BFF]">
+      <div className="w-[400px] bg-[white] [box-shadow:0px_7px_10px_6px_#00000033] rounded-[10px] overflow-hidden">
         <div className="flex justify-center items-center mx-[0] my-[20px] flex-col">
-          <img src={logo.src} alt="Not Available" className="w-[120px]" />
-          <h1 className="font-[Inter] text-[35px] font-bold leading-[60.51px] text-left text-[#407bff] mt-[10px] mb-[10px]">
+          <img src={logo.src} alt="Not Available" className="w-[100px]" />
+          <h1 className="font-[Inter] font-bold text-left text-[#407bff] mt-[5px] mb-[5px] text-[25px]">
             Login
           </h1>
         </div>
 
-        <form>
-          <div>
-            <label htmlFor="EmailAddress">Email Address</label>
-            <input type="email" name="EmailAddress" id="EmailAddress" />
+        <form className="px-[40px] py-[0]">
+          <div className="flex justify-start items-start flex-col mb-[20px]">
+            <label
+              htmlFor="EmailAddress"
+              className="font-[Inter] text-[15px] font-normal leading-[24.2px] text-left text-[#989898] mb-[2px]"
+            >
+              Email Address
+            </label>
+            <input
+              type="email"
+              name="EmailAddress"
+              id="EmailAddress"
+              className="border-[1.5px] border-solid border-[#C7C7C7] w-full p-[10px] rounded-[5px] outline-[none]"
+            />
           </div>
-          <div>
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password" />
+          <div className="flex justify-start items-start flex-col mb-[10px]">
+            <label
+              htmlFor="password"
+              className="font-[Inter] text-[15px] font-normal leading-[24.2px] text-left text-[#989898] mb-[2px]"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              className="border-[1.5px] border-solid border-[#C7C7C7] w-full p-[10px] rounded-[5px] outline-[none]"
+            />
           </div>
-          <Link href="">Forget Password ?</Link>
-          <button type="submit">Submit</button>
-          <button type="submit">
+          <p className="font-[Inter] text-lg font-normal leading-[21.78px] text-[#407afd] text-[15px] text-right">
+            <Link href="">Forget Password ?</Link>
+          </p>
+          <button
+            type="submit"
+            className="w-full font-[Inter] text-[17px] font-medium leading-[26.63px] bg-[#407bff] mt-[10px] p-[10px] rounded-[5px] mb-[10px]"
+          >
+            Submit
+          </button>
+          <button
+            type="submit"
+            className="mb-[20px] border-[1.5px] border-solid border-[#C7C7C7] w-full p-[10px] rounded-[5px] outline-[none] flex justify-center items-center gap-[10px] font-[Inter] text-[15px] font-medium leading-[26.63px] text-left text-[#969696]"
+          >
             <FcGoogle />
             Sign in with google
           </button>
